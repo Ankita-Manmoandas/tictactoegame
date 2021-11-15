1557ad5391318689d39f748da7e5bf68fbed233c
Ankita Manmoandas <ankitamanmoandas@Ankitas-MacBook-Pro.local>
2021-11-15 20:37:48 +0000

    added new files

---
+++ index.js
@@ -0,0 +1,13 @@
+  const cells = document.querySelectorAll (.cell)
+  
+  
+  // when player 1 clicks - x appears , when player 2 clicks 0 appears 
+  let player1;
+  let player2;
+
+  //handlheclick 
+  const handleClick (event) => {
+     cells.innerHTML = "X"
+  }
+
+// player 1 is assinged x, player 2 is assigned 0
