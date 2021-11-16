751fb3e1071cf9a8eaa39d9a75a958399dec8c00
Ankita Manmoandas <ankitamanmoandas@Ankitas-MacBook-Pro.local>
2021-11-16 18:37:51 +0000

    added form and styled it

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
+let player1;
+let player2;
+  
+  
   const cells = document.querySelectorAll (".cell")
+  const submitButton = document.querySelector(".submit")
+  const form = document.querySelector ("#form")
+  const inputForm = document.querySelectorAll (".inputs")
+  const input1 = document.querySelector ("#input1")
+  const input2 = document.querySelector ("#input2")
+  //players can enter their names
+  // when players submit their names- form is replaced with hi player1 and player 2 name. 
+const handleClick = (e) => {
+  player1 = input1.value
+  player2 = input2.value
+  
+if (player1= input1.value ||  (player2 = input2.value)) {
+  form.innerHTML = `It's so nice to meet you ${player1} and ${player2}.  
+  <br>
+  ${player1} you're crosses. 
+  <br> ${player2} you're noughts.
+  <br> Let's play now! `
+
+} else  {
+  alert("Please write your names to start the game")
+
+}} 
+
+
+ submitButton.addEventListener ("click" , handleClick);
+
   
 
   // when player 1 clicks - x appears , when player 2 clicks 0 appears 
-  let player1;
-  let player2;
+
+
+ 
 
   //handlheclick 
 
