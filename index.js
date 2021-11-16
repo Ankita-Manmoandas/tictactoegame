8c580b501cb050f18f354835fa380a9664c82ad0
Ankita Manmoandas <ankitamanmoandas@Ankitas-MacBook-Pro.local>
2021-11-16 09:30:51 +0000

    trying to do a an x interaction

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-  const cells = document.querySelectorAll (.cell)
+  const cells = document.querySelectorAll (".cell")
   
   // show which players turn it is 
-  
+
   // when player 1 clicks - x appears , when player 2 clicks 0 appears 
   let player1;
   let player2;
 
   //handlheclick 
-  const handleClick (e) => {
+  const handleClick = (e) => {
+    cells.innerHTML = "X"
+
    
 // player 1 is assinged x, player 2 is assigned 0
