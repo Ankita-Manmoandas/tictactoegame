73419893fff3cdd5fd6901e3ecf65143b0b7523d
Ankita Manmoandas <ankitamanmoandas@Ankitas-MacBook-Pro.local>
2021-12-05 16:21:17 +0000

    added draw function

--- scripts/index.js
+++ scripts/index.js
@@ -52,49 +52,49 @@ cells.forEach((cell) => {
           form.innerHTML = `${player2}'s turn`
           e.target.innerHTML = "O"
           player1Turn = true;
-        
-        
-
-          
         }
-   
         
       }
 
- 
-  
     // Time to check for a win by calling the checkForWin function :) 
-    // const checkResult = () => {
-    //   if (
-    //     const checkWinner = () => {
-    //     checkForWin1ax(),
-    //     checkForWin1bx (),
-    //     checkForWin1cx (),
-    //     checkForWin2ax (),
-    //     checkForWin2bx (),
-    //     checkForWin2cx (),
-    //     checkForWin3ax (),
-    //     checkForWin3bx ()
-    //     }
-    //     checkWinner ()
-    checkForWinHorizontal (cells[0], cells[1], cells[2]),
-    checkForWinHorizontal (cells[3], cells[4], cells[5])
-  
+   
+    checkForWin (cells[0], cells[1],cells[2]),
+    checkForWin (cells[3], cells[4],cells[5]),
+    checkForWin (cells[6], cells[7],cells[8]),
+    checkForWin (cells[0], cells[3], cells[6]),
+    checkForWin (cells[1], cells[4], cells[7]),
+    checkForWin (cells[2], cells[5], cells[8]),
+    checkForWin (cells[0], cells[4], cells[8]),
+    checkForWin (cells[2], cells[4], cells[6]),
+    checkforDraw ()
     
   });
 
 
+});
+
+//check for draw
+
+const checkforDraw = () => {
+  let grid= "";
+  cells.forEach(cell => {
+    grid += cell.innerHTML
+  })
+  if (grid.length === 9) {
+    alert("its a draw!")
+  }}
+
+
 //winning patterns
-const checkForWinHorizontal = (pattern0, pattern1, pattern2) => {
+const checkForWin = (pattern0, pattern1, pattern2) => {
   if (pattern0.innerHTML=="x" && pattern1.innerHTML=="x" && pattern2.innerHTML== "x")  {
-    alert ("player 1 wins") ;
-   //  form.innerHTML= "";    
+ form.innerHTML= `${player1} wins!! Click the rest button to play again`
    } else if (pattern0.innerHTML=="O" && pattern1.innerHTML=="O" && pattern2.innerHTML== "O")  {
-     alert ("player 2 wins") 
+    form.innerHTML= `${player2} wins!! Click the rest button to play again`
   }}
   
 
-
+//refactored the following functions" 
 
 // //horizontal wins starting from 1a-1c for player 1 (x) only 
 // const checkForWin1ax = () => {
@@ -109,107 +109,6 @@ const checkForWinHorizontal = (pattern0, pattern1, pattern2) => {
 //     console.log ("player 2 wins") 
 //  }}
 
- const checkForWin1bx = () => {
-  const pattern3 = cells[3].innerHTML;
-  const pattern4 = cells[4].innerHTML;
-  const pattern5 = cells[5].innerHTML;
-   
-  if (pattern3=="x" && pattern4=="x" && pattern5== "x")  {
-    alert ("player 1 wins") 
-
-  } else if (pattern3=="O" && pattern4=="O" && pattern5== "O")  {
-   alert ("player 2 wins") 
-}; 
- }
-
- const checkForWin1cx = () => {
-  const pattern6 = cells[6].innerHTML;
-  const pattern7 = cells[7].innerHTML;
-  const pattern8 = cells[8].innerHTML;
-   
-  if (pattern6=="x" && pattern7=="x" && pattern8== "x")  
-  { alert ("player 1 wins") 
-  } else if (pattern6=="O" && pattern7=="O" && pattern8== "O")  {
-    alert  ("player 2 wins") 
-}; 
- }
-
-
-
- // vertical wins= 2a/b/c for player 1 (x)
-
- const checkForWin2ax = () => {
-  const pattern0 = cells[0].innerHTML;
-  const pattern3 = cells[3].innerHTML;
-  const pattern6 = cells[6].innerHTML;
-   
-  if (pattern0=="x" && pattern3=="x" && pattern6== "x")  {
-   alert ("player 1 wins")
-  } else if (pattern0=="O" && pattern3=="O" && pattern6== "O")  {
-    alert ("player 2 wins") 
- 
- 
-   
-  }}
- 
-  const checkForWin2bx = () => {
- 
-   const pattern1 = cells[1].innerHTML;
-   const pattern4 = cells[4].innerHTML;
-   const pattern7 = cells[7].innerHTML;
-    
-   if (pattern1=="x" && pattern4=="x" && pattern7== "x")  {
-     alert ("player 1 wins") 
-    } else if (pattern1=="O" && pattern4=="O" && pattern7== "O")  {
-    alert ("player 2 wins") 
-
-}}; 
-  
- 
-  const checkForWin2cx = () => {
- 
-   const pattern2 = cells[2].innerHTML;
-   const pattern5 = cells[5].innerHTML;
-   const pattern8 = cells[8].innerHTML;
-    
-   if (pattern2=="x" && pattern5=="x" && pattern8== "x")  {
-     alert ("player 1 wins") 
-   } else if (pattern2=="O" && pattern5=="O" && pattern8== "O")  {
-    alert ("player 2 wins") }}
-
-
-   //diagnol wins for player 1 
-   const checkForWin3ax = () => {
-    const pattern0 = cells[0].innerHTML;
-    const pattern4 = cells[4].innerHTML;
-    const pattern8 = cells[8].innerHTML;
-     
-    if (pattern0=="x" && pattern4=="x" && pattern8== "x")  {
-      alert ("player 1 wins")  
-    }
-    else if (pattern0=="O" && pattern4=="O" && pattern8== "O")  {
-      alert ("player 2 wins") }
-  
-  };
-   
-    const checkForWin3bx = () => {
-    const pattern2 = cells[2].innerHTML;
-     const pattern4 = cells[4].innerHTML;
-     const pattern6 = cells[6].innerHTML;
-      
-     if (pattern2=="x" && pattern4=="x" && pattern6== "x")  {
-       alert ("player 1 wins") 
-   }
-   else if (pattern2=="O" && pattern4=="O" && pattern6== "O")  {
-   alert("player 2 wins") }
-  }})
-// // check for draw
-// checkForDraw= () => {
-//   if (cells== "x" || "o" &&)
-
-
-
-
 
   //reset button
   let handleClickReset = () => {
@@ -217,8 +116,8 @@ const checkForWinHorizontal = (pattern0, pattern1, pattern2) => {
 
     function resetFunction (cells)  {
       cells.innerHTML= ("")
-    }
-    
+
+    }    
     
   }
   resetButton.addEventListener("click", handleClickReset); 
